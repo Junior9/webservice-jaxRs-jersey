386827653616f528b076d452997992b97ada936b
Francisco Carlos Moraes Junior <tc.fjunior@padtec.com.br>
2017-07-19 08:53:22 -0300

    Melhorando os testes com carrinhos

--- loja/loja/src/main/java/br/com/alura/loja/Servidor.java
+++ loja/loja/src/main/java/br/com/alura/loja/Servidor.java
@@ -1,6 +1,5 @@
 package br.com.alura.loja;
 
-import java.io.IOException;
 import java.net.URI;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
@@ -8,13 +7,13 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class Servidor {
   
-  public static void main(String[] args) throws IOException {
-    
+  private static HttpServer server = null;
+  
+  public static HttpServer startaServidor() {
     ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
     URI uri = URI.create("http://localhost:8090/");
-    HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri,config);
+    server = GrizzlyHttpServerFactory.createHttpServer(uri,config);
     System.out.println("Servidor Rodando");
-    System.in.read();
-    server.stop();
+    return server;
   }
 }
--- loja/loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ loja/loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -4,16 +4,33 @@ import static org.junit.Assert.assertTrue;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import com.thoughtworks.xstream.XStream;
+import br.com.alura.loja.modelo.Carrinho;
 
 public class ClienteTest {
 
-  @Test
-  public void testaConexaoComServidor() {
-    Client client = ClientBuilder.newClient();
-    WebTarget target = client.target("http://www.mocky.io/");
-    String conteudo =  target.path("v2/52aaf5bbee7ba8c60329fb7b").request().get(String.class);
-    assertTrue(conteudo.contains("Rua Vergueiro 3185"));
+  private HttpServer server;
+  
+  @Before
+  public void initTest(){
+    server = Servidor.startaServidor();
   }
   
+  @After
+  public void endTest(){
+    server.stop();
+  }
+  
+  @Test
+  public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+    Client client = ClientBuilder.newClient();
+    WebTarget target = client.target("http://localhost:8090");
+    String conteudo =  target.path("/carrinhos").request().get(String.class);
+    Carrinho carrinho = (Carrinho) new  XStream().fromXML(conteudo);
+    assertTrue(carrinho.getRua().contains("Rua Vergueiro 3185, 8 andar"));
+  }
 }
